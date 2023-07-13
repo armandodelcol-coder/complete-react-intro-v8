d23bc74147ad0af56c1694c968849d1d59957fd6
armandodelcol-coder <atdc.codemaster@gmail.com>
2023-07-13 19:49:42 -0300

    useState hook

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,11 @@
-import { createRoot } from "react-dom";
-import Pet from "./Pet";
+import { createRoot } from "react-dom/client";
+import SearchParams from "./SearchParams";
 
 const App = () => {
   return (
     <div>
       <h1>Adopt Me!</h1>
-      <Pet name="Bili" animal="Dog" breed="Mixed" />
-      <Pet name="Jack" animal="Dog" breed="Jack Houssel" />
-      <Pet name="Toby" animal="Dog" breed="Mixed" />
+      <SearchParams />
     </div>
   );
 };

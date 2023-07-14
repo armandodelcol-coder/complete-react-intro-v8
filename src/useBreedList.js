63bfde4dece902505c0df51c6011c683f87a1b9a
armandodelcol-coder <atdc.codemaster@gmail.com>
2023-07-14 19:57:45 -0300

    first custom hook

---
+++ src/useBreedList.js
@@ -0,0 +1,35 @@
+import { useState, useEffect } from "react";
+
+const localCache = {};
+
+export default function useBreedList(animal) {
+  const [breedlist, setBreedList] = useState([]);
+  const [status, setStatus] = useState("unloaded");
+
+  useEffect(() => {
+    if (!animal) {
+      setBreedList([]);
+    } else if (localCache[animal]) {
+      setBreedList(localCache[animal]);
+    } else {
+      requestBreedList();
+    }
+
+    // The requestBreedList defined inside useEffect makes it's not recreated on every render cycle
+
+    async function requestBreedList() {
+      setBreedList([]);
+      setStatus("loading");
+
+      const res = await fetch(
+        `http://pets-v2.dev-apis.com/breeds?animal=${animal}`
+      );
+      const json = await res.json();
+      localCache[animal] = json.breeds || [];
+      setBreedList(localCache[animal]);
+      setStatus("loaded");
+    }
+  }, [animal]);
+
+  return [breedlist, status];
+}
